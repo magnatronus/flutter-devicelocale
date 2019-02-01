310febdfc852ec3aef3cc2e57c05857e0e72bcb7
SpiralArm Consulting Ltd <projects@spiralarm.uk>
2019-02-01 15:27:30 +0000

    document and licence update

--- lib/devicelocale.dart
+++ lib/devicelocale.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
 
+// Copyright (c) 2019, Steve Rogers. All rights reserved. Use of this source code
+// is governed by an Apache License 2.0 that can be found in the LICENSE file.
 import 'package:flutter/services.dart';
 
+/// A Simple plug-in that can be used to interogate a device( iOS or Android) to obtain a list of current set up locales
+/// the currently used locale should be the first in the list
 class Devicelocale {
   static const MethodChannel _channel = const MethodChannel('uk.spiralarm.flutter/devicelocale');
 
+  /// Get a String List of locales from the device
   static Future<List> get preferredLanguages async {
     final List version = await _channel.invokeMethod('preferredLanguages');
     return version;
